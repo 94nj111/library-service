2a00babfaba56f4e2122ac7ade0af34b6ab3cdcb
Dmytro <94nj111@gmail.com>
2025-01-02 12:38:46 +0200

    fix core project directory

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&x9ajn#1)9%b56fcnfjvm&%$m954c0mwkn66qij0qv5i$2cd3)"
+SECRET_KEY = "django-insecure-2ge!m5+o!ct1!fwj(#2_)^7d8u_=_++z8$56pdge1s%udc!t@h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
