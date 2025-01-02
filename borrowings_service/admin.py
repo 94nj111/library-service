556539d52eebf5422e503c32788893607fdb9eab
Andrey Dementyev <dementyev.andreyy@gmail.com>
2025-01-02 19:26:32 +0200

    registered Borrowing model in admin.py

--- borrowings_service/admin.py
+++ borrowings_service/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Borrowing
 
-# Register your models here.
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "book",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+        "is_active",
+    )
+
+    list_filter = ("user", "borrow_date", "actual_return_date", "book")
+
+    ordering = ("-borrow_date", "expected_return_date")
+
+    search_fields = ("user__email", "book__title")
+
+    readonly_fields = ("is_active",)
+
+    def is_active(self, obj):
+        return obj.is_active
+
+    is_active.boolean = True
