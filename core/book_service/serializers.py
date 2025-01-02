6831252c1434e57d205162344c429c463a69d11f
Leon Kushnir <leon.kushnir15@gmail.com>
2025-01-02 13:55:21 +0200

    add fields to serializer

--- core/book_service/serializers.py
+++ core/book_service/serializers.py
@@ -6,3 +6,11 @@ from core.book_service.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_free",
+        )
