7026e2cd3626542e3bc8045c067a408bc096cd28
Leon Kushnir <leon.kushnir15@gmail.com>
2025-01-02 15:52:12 +0200

    move file core/book_service --> (root) library-service

--- core/book_service/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from core.book_service.models import Book
-
-admin.site.register(Book)
--- core/book_service/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from core.book_service.models import Book
-from core.book_service.serializers import BookSerializer
-
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
