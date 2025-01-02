59e8a6fa333baf338a2cde1a48a708ddde935be2
Andrey Dementyev <dementyev.andreyy@gmail.com>
2025-01-02 17:33:32 +0200

    added Borrowing model

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from book_service.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(default=timezone.now)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @property
+    def is_active(self):
+        return self.actual_return_date is None
+
+    def __str__(self):
+        return f"Borrowing(User: {self.user}, book: {self.book.title}, borrow_date: {self.borrow_date})"
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(expected_return_date__gte=models.F("borrow_date")),
+                name="expected_return_after_borrow",
+            )
+        ]
