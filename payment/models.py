19a2c9f875660852ed45386343ff0589e8c21bbb
Den_k0 <denys.kotovskiy@gmail.com>
2025-01-03 11:29:14 +0200

    Changed borrowing IntegerField to ForeignKey in Payment model

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from borrowings_service.models import Borrowing
+
 STATUS_CHOICES = (
     ("PENDING", "Pending"),
     ("PAID", "Paid"),
@@ -14,7 +16,7 @@ TYPE_CHOICES = (
 class Payment(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
-    borrowing = models.IntegerField()
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
