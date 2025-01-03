e249c5f042ffaadb08fea28d5610f64693aec4c5
user <nikitaskrypets@gmail.com>
2025-01-03 15:59:03 +0200

    Move imports inside function

--- borrowings_service/signals.py
+++ borrowings_service/signals.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from borrowings_service.models import Borrowing
-from library_bot.bot import bot, get_users
 
 
 @receiver(post_save, sender=Borrowing)
 async def send_notification_on_borrowing_created(sender, instance, created, **kwargs):
     if created:
+        from library_bot.bot import bot, get_users
+
         borrow_date = datetime.strftime(instance.borrow_date, "%Y-%m-%d %H:%M:%S")
         text = (
             f"New borrowing was created:\n"
