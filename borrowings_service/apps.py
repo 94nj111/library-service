4e2a403f6739fc4709e1736b78ec1b679a045127
user <nikitaskrypets@gmail.com>
2025-01-03 14:13:16 +0200

    Create signal for notifications on borrowing created

--- borrowings_service/apps.py
+++ borrowings_service/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BorrowingsServiceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "borrowings_service"
+
+    def ready(self):
+        import borrowings_service.signals # noqa
---
+++ borrowings_service/signals.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from borrowings_service.models import Borrowing
+from library_bot.bot import bot, get_users
+
+
+@receiver(post_save, sender=Borrowing)
+async def send_notification_on_borrowing_created(sender, instance, created, **kwargs):
+    if created:
+        borrow_date = datetime.strftime(instance.borrow_date, "%Y-%m-%d %H:%M:%S")
+        text = (f"New borrowing was created:\n"
+                f"Borrow date: {borrow_date}\n"
+                f"Expected return date: {instance.expected_return_date}\n"
+                f"Book: {instance.book.title}\n"
+                f"User email: {instance.user.email}")
+
+        for user_id in await get_users():
+            await bot.send_message(user_id, text)
