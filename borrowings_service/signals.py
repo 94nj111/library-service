3357836535682e2ba25cb6feddc07c91e02d25a0
user <nikitaskrypets@gmail.com>
2025-01-03 15:01:27 +0200

    Refactored with black

--- borrowings_service/signals.py
+++ borrowings_service/signals.py
@@ -10,11 +10,13 @@ from library_bot.bot import bot, get_users
 async def send_notification_on_borrowing_created(sender, instance, created, **kwargs):
     if created:
         borrow_date = datetime.strftime(instance.borrow_date, "%Y-%m-%d %H:%M:%S")
-        text = (f"New borrowing was created:\n"
-                f"Borrow date: {borrow_date}\n"
-                f"Expected return date: {instance.expected_return_date}\n"
-                f"Book: {instance.book.title}\n"
-                f"User email: {instance.user.email}")
+        text = (
+            f"New borrowing was created:\n"
+            f"Borrow date: {borrow_date}\n"
+            f"Expected return date: {instance.expected_return_date}\n"
+            f"Book: {instance.book.title}\n"
+            f"User email: {instance.user.email}"
+        )
 
         for user_id in await get_users():
             await bot.send_message(user_id, text)
