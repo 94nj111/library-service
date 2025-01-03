302b4d04ab5b3d30ec87e98156ae081fb68d4ae0
Den_k0 <denys.kotovskiy@gmail.com>
2025-01-03 17:38:36 +0200

    Implement views for payment app

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        """ Admin see all payments, user - only his own """
+
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(borrowing__user=user)
