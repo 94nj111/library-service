103c9b9c442b9ee8c124ea0dab6480a7a342889e
Leon Kushnir <leon.kushnir15@gmail.com>
2025-01-04 11:30:26 +0200

    delete cache in payment views

--- payment/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
-
-# @method_decorator(cache_page(60 * 5, key_prefix="payment_view"))
-#     def dispatch(self, request, *args, **kwargs):
-#         return super().dispatch(request, *args, **kwargs)
